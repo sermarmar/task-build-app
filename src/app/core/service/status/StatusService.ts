d2df2f726b25de4077feabf72d88bce35f41e32b
sermarmar <sermarmar1995@gmail.com>
2026-04-15 16:05:47 +0200

    feat: update ModalCreateTask to fetch category and status on show, improving task creation flow

--- src/app/core/service/status/StatusService.ts
+++ src/app/core/service/status/StatusService.ts
@@ -1,9 +1,10 @@
 import { supabase } from "../../../../config/Database";
+import type { ErrorMessage } from "../../../shared/Error";
 import type { Status } from "../../models/Status";
 
 export const StatusService = {
 
-    getAllStatus: async (): Promise<{ status: Status[] | null, error: any }> => {
+    getAllStatus: async (): Promise<{ status: Status[] | null, error: ErrorMessage | null }> => {
         
         if(sessionStorage.getItem('status')) {
             return { status: JSON.parse(sessionStorage.getItem('status')!), error: null };
@@ -11,10 +12,10 @@ export const StatusService = {
             const { data, error } = await supabase.from('statuses').select('*');
 
             if (error) {
-                return { status: null, error };
+                return { status: null, error: { message: 'No se ha podido obtener los estados' } };
             }
 
-            const statuses: Status[] = data.map((status: any) => ({
+            const statuses: Status[] = data.map((status) => ({
                 id: status.id,
                 name: status.name,
                 description: status.description,
@@ -29,12 +30,12 @@ export const StatusService = {
         
     },
 
-    getFirstStatus: async (): Promise<{ status: Status | null, error: any }> => {
+    getFirstStatus: async (): Promise<{ status: Status | null, error: ErrorMessage | null }> => {
         const statusJson = JSON.parse(sessionStorage.getItem('status') || 'null');
         if (statusJson && statusJson.length > 0) {
             return { status: statusJson[0], error: null };
         }
-        return { status: null, error: null };
+        return { status: null, error: { message: 'No status found in session storage' } };
     }
 
 }
