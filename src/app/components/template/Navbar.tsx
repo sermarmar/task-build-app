dd5dfa253aeda82a9f99fe7207ffb584a07eb501
sermarmar <sermarmar1995@gmail.com>
2026-04-24 11:16:06 +0200

    feat: mejorar Navbar y Button con nuevos estilos y funcionalidad de navegación

--- src/app/components/template/Navbar.tsx
+++ src/app/components/template/Navbar.tsx
@@ -1,6 +1,7 @@
 import { Bolt, CircleUserRound, House, LogOut, Notebook, User } from "lucide-react";
 import { useAuth } from "../../contexts/auth/useAuth";
-import { useNavigate } from "react-router";
+import { useLocation, useNavigate } from "react-router";
+import { Button } from "../ux/Button";
 
 interface NavbarProps {
     className?: string;
@@ -10,23 +11,49 @@ export const Navbar: React.FC<NavbarProps> = ({ className }) => {
 
     const { user, logout } = useAuth();
     const navigate = useNavigate();
+    const { pathname } = useLocation();
+
+    const navItems = [
+        { icon: <House />, path: '/home' },
+        { icon: <Notebook />, path: '/notes' },
+        { icon: <CircleUserRound />, path: '/profile' },
+        { icon: <Bolt />, path: '/settings' },
+    ];
+
+    const isActive = (path: string) => pathname === path;
 
     const handleLogout = () => {
         logout();
         navigate('/');
     };
 
+
     return (
-        <nav className={`bg-tertiary-200 shadow rounded-2xl text-primary-900 h-full flex flex-col items-center ${className}`}>
-            <div className="flex flex-col h-full items-center justify-between p-4">
+        <nav className={`bg-tertiary-200 shadow rounded-l-2xl rounded-e-4xl text-primary-900 h-full flex flex-col items-center ${className}`}>
+            <div className="flex flex-col h-full items-center justify-between p-2">
                 <User size={24} className="mb-2" />
                 <div className="flex flex-col gap-8">
-                    <House />
-                    <Notebook />
-                    <CircleUserRound />
-                    <Bolt />
+                    {navItems.map(({ icon, path }) => (
+                        <Button
+                            key={path}
+                            type="button"
+                            form="pill"
+                            color="transparent"
+                            onClick={() => navigate(path)}
+                            className={`
+                                hover:bg-primary-900 hover:text-tertiary-50
+                                ${isActive(path)
+                                    ? 'bg-primary-900 text-tertiary-50' 
+                                    : ''}
+                            `}
+                        >
+                            {icon}
+                        </Button>
+                    ))}
                 </div>
-                <LogOut size={16} className="mb-2" onClick={ handleLogout }/>
+                <Button type="button" form="pill" color="transparent" onClick={ handleLogout } className="hover:bg-primary-900 hover:text-tertiary-50">
+                    <LogOut />
+                </Button>
             </div>
         </nav>
     );
--- src/app/components/ux/Button.tsx
+++ src/app/components/ux/Button.tsx
@@ -1,20 +1,38 @@
+import { twMerge } from "tailwind-merge";
+
 interface ButtonProps {
     children: React.ReactNode;
     onClick?: () => void;
     className?: string;
     type: 'button' | 'submit' | 'reset';
+    form?: 'square' | 'rounded' | 'pill';
     disabled?: boolean;
-    color?: 'primary' | 'secondary' | 'danger' | 'warning' | 'success' | 'info' | 'light' | 'dark';
+    color?: 'primary' | 'secondary' | 'tertiary' | 'transparent' | 'danger' | 'warning' | 'success' | 'info' | 'light' | 'dark';
 }
 
-export const Button: React.FC<ButtonProps> = ({ children, onClick, className, type, disabled, color = 'primary' }) => {
+export const Button: React.FC<ButtonProps> = ({ children, onClick, className, type, form = 'square', disabled, color = 'primary' }) => {
 
     const getColorClass = () => {
         switch (color) {
             case 'primary':
-                return 'bg-gradient-to-r from-accent-from to-accent-to';
+                return 'bg-primary-900 text-tertiary-50';
             case 'secondary':
-                return 'bg-gray-500';
+                return 'bg-secondary-500 text-tertiary-50';
+            case 'tertiary':
+                return 'bg-tertiary-500 text-tertiary-50';
+            case 'transparent':
+                return 'bg-transparent text-primary-900';
+        }
+    }
+
+    const getFormClass = () => {
+        switch (form) {
+            case 'square':
+                return 'rounded-md px-4 py-2';
+            case 'rounded':
+                return 'rounded-lg px-4 py-2';
+            case 'pill':
+                return 'rounded-full p-2';
         }
     }
 
@@ -23,7 +41,7 @@ export const Button: React.FC<ButtonProps> = ({ children, onClick, className, ty
             type={type} 
             onClick={onClick}
             disabled={disabled}
-            className={`${ getColorClass() } flex items-center gap-2 text-white px-4 py-2 rounded-lg font-semibold hover:from-purple-700 hover:to-pink-700 transform hover:scale-105 transition duration-200 shadow-lg ${className}`}>
+            className={twMerge( getColorClass(), 'flex items-center gap-2 cursor-pointer', getFormClass(), 'font-semibold transform hover:scale-105 transition duration-200', className )}>
             { children }
         </button>
     )
