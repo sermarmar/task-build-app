25cb885af8586643ef3e0223331c9338f239369a
sermarmar <sermarmar1995@gmail.com>
2026-04-28 15:52:34 +0200

    feat: agregar rutas para perfil y configuración en RouterProvider

--- src/app/routes/RouteProvider.tsx
+++ src/app/routes/RouteProvider.tsx
@@ -5,6 +5,7 @@ import { ProtectedRoute } from "../components/ProtectedRoute";
 import { AuthProvider } from "../contexts/auth/AuthProvider";
 import { TaskPage } from "../pages/TaskPage";
 import { Navbar } from "../components/template/Navbar";
+import { ConfigPage } from "../pages/ConfigPage";
 
 export const RouterProvider: React.FC = () => {
     return (
@@ -17,14 +18,16 @@ export const RouterProvider: React.FC = () => {
                     {/* Rutas privadas - con navbar */}
                     <Route path="/*" element={
                         <ProtectedRoute element={
-                            <div className="flex h-screen overflow-hidden">
+                            <div className="flex h-screen overflow-hidden w-full">
                                 <div className="flex-none md:m-5">
                                     <Navbar className="" />
                                 </div>
-                                <div className="my-10 pr-5 overflow-y-auto scrollbar-primary">
+                                <div className="my-10 pr-5 overflow-y-auto scrollbar-primary flex-1">
                                     <Routes>
                                         <Route path="/home" element={<DashboardPage />} />
                                         <Route path="/notes" element={<TaskPage />} />
+                                        <Route path="/profile" element={<div>Perfil</div>} />
+                                        <Route path="/settings" element={<ConfigPage />} />
                                     </Routes>
                                 </div>
                             </div>
