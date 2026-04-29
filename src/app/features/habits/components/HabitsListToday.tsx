bf2e8222ad93493cef8d276249e93b8f0cba8c36
sermarmar <sermarmar1995@gmail.com>
2026-04-29 16:22:15 +0200

    feat: simplificar la integración de HabitsListToday en DashboardPage

--- src/app/features/habits/components/HabitsListToday.tsx
+++ src/app/features/habits/components/HabitsListToday.tsx
@@ -1,8 +1,9 @@
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { RetrieveHabitsService } from "../services/RetrieveHabitsService";
 import type { Habit } from "../models/Habit";
 import { DynamicIcon } from "../../../components/ux/DynamicIcon";
 import { useColorAlpha } from "../../../hooks/useColorAlpha";
+import { Card } from "../../../components/ux/Card";
 
 export const HabitsListToday: React.FC = () => {
 
@@ -21,31 +22,36 @@ export const HabitsListToday: React.FC = () => {
 
 
     return (
-        <>
-            {error && <div className="error">{error}</div>}
-            {
-                habits.length === 0 && !error && 
-                <div className='text-gray-400 font-bold text-5xl text-center mt-5'>No hay hábitos disponibles</div>
-            }
-            <div className="grid grid-cols-1 gap-4">
-                {habits
-                .map(habit => (
-                    <div className="flex gap-4 items-center p-4 rounded-md border-l-5 border-tertiary-200 bg-tertiary-600/45 w-full"
-                        style={{
-                            backgroundColor: useColorAlpha(habit.categories?.color, 0.2),
-                            borderColor: habit.categories?.color,
-                        }}>
-                        <span className="text-tertiary-50 w-10 h-10 flex items-center justify-center rounded-full"
+        
+        <div className="max-h-full flex flex-col">
+            <h3 className="text-lg font-medium ml-4 mb-2">Hábitos para hoy</h3>
+            <Card>
+                {error && <div className="error">{error}</div>}
+                {
+                    habits.length === 0 && !error && 
+                    <div className='text-gray-400 font-bold text-5xl text-center mt-5'>No hay hábitos disponibles</div>
+                }
+                <div className="h-65 grid grid-cols-1 gap-4 overflow-y-auto overflow-x-hidden">
+                    {habits
+                    .map(habit => (
+                        <div className="flex gap-4 items-center p-4 rounded-md border-l-5 border-tertiary-200 bg-tertiary-600/45 w-full"
                             style={{
-                                backgroundColor: habit.categories?.color,
+                                backgroundColor: useColorAlpha(habit.categories?.color, 0.2),
+                                borderColor: habit.categories?.color,
                             }}>
-                            <DynamicIcon name={habit.categories?.icon} />
-                        </span>
-                        <h3 className="text-md font-medium">{habit.title}</h3>
-                    </div>
-                ))}
-            </div>
-        </>
+                            <span className="text-tertiary-50 w-10 h-10 flex items-center justify-center rounded-full"
+                                style={{
+                                    backgroundColor: habit.categories?.color,
+                                }}>
+                                <DynamicIcon name={habit.categories?.icon} />
+                            </span>
+                            <h3 className="text-md font-medium">{habit.title}</h3>
+                        </div>
+                    ))}
+                </div>
+            </Card>
+        </div>
+        
     );
 
 }
